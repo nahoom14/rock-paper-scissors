79b70d4fa39fddc619f0280710c97600597c8149
Nahoom Belete <nahoombelete22@gmail.com>
2022-12-01 23:58:34 +0200

    Add getComputerChoice() function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,10 @@
-console.log("hello");
+// Different choices
+const arrChoices = ["Rock", "Paper", "Scissors"];
+
+// getComputerChoice() function call
+console.log(getComputerChoice());
+
+// Returns one of the three choices from arrChoices
+function getComputerChoice(){
+    return arrChoices[Math.floor(Math.random() * 3)];
+}
